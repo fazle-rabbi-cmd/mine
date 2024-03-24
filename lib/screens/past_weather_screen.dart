9e25d220107ede26c3f5fefac3d1b81232ac8798
fazle-rabbi-cmd <hridoy3761@gmail.com>
2024-03-24 15:23:14 +0600

    past weather almost done

--- lib/screens/past_weather_screen.dart
+++ lib/screens/past_weather_screen.dart
@@ -1,80 +1,3 @@
-// import 'package:flutter/material.dart';
-// import '../services/past_weather_service.dart';
-//
-// class PastWeatherScreen extends StatefulWidget {
-//   @override
-//   _PastWeatherScreenState createState() => _PastWeatherScreenState();
-// }
-//
-// class _PastWeatherScreenState extends State<PastWeatherScreen> {
-//   final WeatherService weatherService = WeatherService();
-//   Map<String, dynamic> weatherData = {};
-//   TextEditingController _dateController = TextEditingController();
-//   TextEditingController _locationController = TextEditingController();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//   }
-//
-//   Future<void> fetchWeatherData(String location, String date) async {
-//     try {
-//       final data = await weatherService.getPastWeatherData(location, date);
-//       setState(() {
-//         weatherData = data;
-//
-//       });
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Past Weather Data'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextField(
-//               controller: _dateController,
-//               decoration: InputDecoration(
-//                 labelText: 'Enter Date (YYYY-MM-DD)',
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             TextField(
-//               controller: _locationController,
-//               decoration: InputDecoration(
-//                 labelText: 'Enter Location',
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 fetchWeatherData(_locationController.text, _dateController.text);
-//               },
-//               child: Text('Search'),
-//             ),
-//             SizedBox(height: 20),
-//             Expanded(
-//               child: SingleChildScrollView(
-//                 child: Text(
-//                   weatherData.isNotEmpty ? weatherData.toString() : 'No data available',
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
 import 'package:flutter/material.dart';
 import '../services/past_weather_service.dart';
 
@@ -101,7 +24,7 @@ class _PastWeatherScreenState extends State<PastWeatherScreen> {
         weatherData = data;
       });
     } catch (e) {
-      print(e);
+      print('Error fetching weather data: $e');
     }
   }
 
@@ -139,19 +62,15 @@ class _PastWeatherScreenState extends State<PastWeatherScreen> {
             SizedBox(height: 20),
             Expanded(
               child: SingleChildScrollView(
-                child: weatherData.isNotEmpty
-                    ? Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: weatherData.entries
-                      .map(
-                        (entry) => WeatherPropertyWidget(
-                      propertyName: entry.key,
-                      propertyValue: entry.value.toString(),
-                    ),
-                  )
-                      .toList(),
-                )
-                    : Text('No data available'),
+                  children: [
+                    if (weatherData.isNotEmpty)
+                      ..._buildWeatherDataList(weatherData)
+                    else
+                      Text('No data available'),
+                  ],
+                ),
               ),
             ),
           ],
@@ -159,41 +78,43 @@ class _PastWeatherScreenState extends State<PastWeatherScreen> {
       ),
     );
   }
-}
 
-class WeatherPropertyWidget extends StatelessWidget {
-  final String propertyName;
-  final String propertyValue;
-
-  const WeatherPropertyWidget({
-    Key? key,
-    required this.propertyName,
-    required this.propertyValue,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(
-              '$propertyName: ',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Text(
-              propertyValue,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
-      ),
-    );
+  List<Widget> _buildWeatherDataList(Map<String, dynamic> data) {
+    final List<dynamic> dataList = data['data'] ?? [];
+    return dataList.map((item) => WeatherItemWidget(item: item)).toList();
   }
 }
 
+class WeatherItemWidget extends StatelessWidget {
+  final Map<String, dynamic> item;
+
+  const WeatherItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: item.entries.map((entry) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '${entry.key}:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  entry.value.toString(),
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
